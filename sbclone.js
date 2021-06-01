049e83f7ac1cc0cfdd1ee555a63633107aa06bea
Kharza <zelkutay@gmail.com>
2021-06-02 00:58:13 +0300

    second commit

---
+++ sbclone.js
@@ -0,0 +1,17 @@
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+window.onscroll = function(){
+    myFunction()
+};
+
+function myFunction(){
+    if(window.pageYOffset > sticky)
+    {
+        header.classList.add("sticky");
+    } 
+    else
+    {
+        header.classList.remove("sticky");
+    }
+}
