1887ec7c52114fd7e52fc7682194a9830fa7d560
Kharza <zelkutay@gmail.com>
2021-06-06 20:55:50 +0300

    tab changes

--- sbclone.js
+++ sbclone.js
@@ -1,5 +1,7 @@
 var header = document.getElementById("myHeader");
 var sticky = header.offsetTop;
+var btnContainer = document.querySelector('.tablist');
+var btns = btnContainer.getElementsByClassName('tabpoints');
 
 window.onscroll = function(){
     myFunction()
@@ -15,3 +17,20 @@ function myFunction(){
         header.classList.remove("sticky");
     }
 }
+
+function openTab(evt, drinkSize){
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("drink");
+    for (i = 0; i < tabcontent.length; i++) 
+    {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tabpoints");
+    for (i = 0; i < tablinks.length; i++) 
+    {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(drinkSize).style.display = "block";
+    evt.currentTarget.className += "active";
+}
+
