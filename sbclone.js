7aa4184bd4dd8abae27f99eec37a49e5f354e299
Kharza <zelkutay@gmail.com>
2021-06-25 12:23:59 +0300

    trying carousel things

--- sbclone.js
+++ sbclone.js
@@ -62,3 +62,74 @@ window.onclick = function(event) {
 /*function openModal(){
     btn.onclick =modal.style.display = "flex";
 }*/
+
+var track = document.querySelector(".modallist");
+const slides = Array.from(track.children);
+const nextButton = document.querySelector(".carousel.ok.right");
+const prevButton = document.querySelector(".carousel.ok.left");
+const dotsNav = document.querySelector('.sliders');
+const dots = Array.from(dotsNav.children);
+
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+slides[0].style.left = '0px';
+slides[1].style.left = '300px';
+slides[2].style.left = '600px';
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = 'translateX(-' + targetSlide.style.left +')';
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+}
+
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove('current-slide');
+    targetDot.classList.add('current-slide');
+}
+
+prevButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const prevDot = currentDot.previousElementSibling;
+    
+    moveToSlide(track, currentSlide, prevSlide);
+    updateDots(currentDot, prevDot);
+});
+
+nextButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const nextDot = currentDot.nextElementSibling;
+   
+    moveToSlide(track, currentSlide, nextSlide);
+    updateDots(currentDot, nextDot);
+});
+
+dotsNav.addEventListener('click', e => {
+    // what indicator was clicked on?
+    const targetDot = e.target.closest('button');
+
+    if (!targetDot) return;
+
+    const currentSlide = track.querySelector('.current-slide');
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const targetIndex = dots.findIndex(dot => dot === targetDot);
+    const targetSlide = slides[targetIndex - 1];
+
+    moveToSlide(track, currentSlide, targetSlide);
+    updateDots(currentDot, targetDot);
+
+    if(targetIndex === 1){
+        prevButton.classList.add('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    } else if(targetIndex === slides.length - 2) {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.add('is-hidden');
+    } else{
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    }
+})
